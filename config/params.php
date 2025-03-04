45db3ea7d2bdcbe8cc7dd7261aeac975d03abdf8
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2025-03-04 14:59:40 +0100

    add authentication layout and update login view

--- config/params.php
+++ config/params.php
@@ -6,5 +6,4 @@ return [
     'senderName' => 'Example.com mailer',
     'bsVersion' => '5.x',
     'icon-framework' => \kartik\icons\Icon::FAS,
-    'googleMapsApiKey' => 'AIzaSyAF4Mv0TAxV6w2i8AxmpTurO4qTbhPJrh0',
 ];
